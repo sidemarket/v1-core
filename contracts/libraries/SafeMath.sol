fe1023e1f952ac2da4380c11a7993549081fbc02
andresbytes <giantquartz@gmail.com>
2023-03-04 16:04:19 +0100

    Start implementing SafeMath

---
+++ contracts/libraries/SafeMath.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.6.0) (utils/math/SafeMath.sol)
+
+pragma solidity ^0.8.0;
+
+library SafeMath {
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+}
